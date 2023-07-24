6d9294f3ad1ffefe72d6a244ee424f7a9149bdff
jackcdavey <jdavey@scu.edu>
2023-07-24 12:54:16 -0700

    Initial Commit

---
+++ ProtoCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProtoCalc
+//
+//  Created by Jack Davey on 7/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProtoCalc/ProtoCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoCalcApp.swift
+//  ProtoCalc
+//
+//  Created by Jack Davey on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ProtoCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
