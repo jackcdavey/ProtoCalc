3880099fdf33095efbc61431b09c45dfccca5010
jackcdavey <jdavey@scu.edu>
2023-07-25 13:49:39 -0700

    added delete key

--- ProtoCalc/ContentView.swift
+++ ProtoCalc/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var input = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        HStack{
+            Spacer()
+            Text(input).bold()
+            Spacer()
+//            Delete button
+            Button(
+                action: {
+                    if(input.count > 0){
+                        input.removeLast()
+                    }
+                }){
+                    Image(systemName: "delete.left")
+                }
+            .buttonStyle(.bordered)
+            
+            Spacer()
+            
         }
-        .padding()
-    }
+        VStack {
+            VStack{
+                
+                ForEach(0..<3){ j in
+                    HStack{
+                        ForEach(1 ..< 4) { i in
+                            Button("\(i + (j*3))", action: {
+                                input += "\(i + (j*3))"
+                                let impactMed = UIImpactFeedbackGenerator(style: .medium)
+                                    impactMed.impactOccurred()
+                            }).buttonStyle(.bordered)
+                        }
+                    }
+                }
+                HStack{
+                    Button("0", action:{
+                        input += "0"
+                    }).buttonStyle(.bordered)
+                }
+                
+                Button("CLEAR", role: .destructive, action:{
+                    input = ""
+                })
+                }
+            }
+        }
+    
 }
 
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
+                   
